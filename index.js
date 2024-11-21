095a1f3d6a0b91153a1473d07051b8547fbb843f
Tola-lemma <tolalemma@gmail.com>
2024-11-21 19:01:06 +0300

    feat:.env is implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const PORT = 3000;
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
@@ -8,4 +9,11 @@ app.get('/',(req,res)=>{
       res.send("Welcome to Book Collection API")
 })
 
+// Custom 404 Error Handler for invalid routes
+app.use((req, res) => {
+      res.status(404).json({
+        error: "Sorry Route not found!",
+        message: `The route '${req.originalUrl}' does not exist on this server. Please check your request and try again.`
+      });
+    });
 app.listen(PORT,()=>console.log(`Server is running on Port number ${PORT}...`))
