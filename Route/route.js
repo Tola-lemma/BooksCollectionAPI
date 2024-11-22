c750d8c2d817b7b98055fab6084915a2d17635bf
Tola-lemma <tolalemma@gmail.com>
2024-11-23 00:05:11 +0300

    Feat:get /books/best/recommendations api endpoint is added that recommend best three books

--- Route/route.js
+++ Route/route.js
@@ -4,9 +4,11 @@ const books  = require('../Controller/BooksController');
 const getAllBooks = require('../Controller/GetAllBooks')
 const getBooksById = require('../Controller/GetBooksById')
 const coverImg = require('../Controller/GetBooksCoverImgById')
+const bookrecommendation = require('../Controller/BooksRecommendations')
 //API end-points
 router.post('/books',books.createBooks)
 router.get('/books',getAllBooks.GetAllBooks)
-router.get('/books/:id?',getBooksById.GetBookById) 
+router.get('/books/:id',getBooksById.GetBookById) 
 router.get('/books/cover/:id',coverImg.GetBookCoverImgById)
+router.get('/books/best/recommendations',bookrecommendation.bookRecommendation)
 module.exports = router
