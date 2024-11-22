31c0fc8ea749b768694e48492a7e78e858dac4f2
Tola-lemma <tolalemma@gmail.com>
2024-11-22 22:05:30 +0300

    feat:returning all available books from datatbase

---
+++ Controller/GetAllBooks.js
@@ -0,0 +1,21 @@
+const db = require('../Database/connection');
+
+exports.GetAllBooks = async (req, res) => {
+  const query = 'SELECT * FROM books;';
+  try {
+    const result = await db.query(query);
+    const books = result.rows.map(book => {
+      if (book.cover_img) {
+        const fileName = `cover_${book.id}.jpg`; 
+        book.cover_img = { fileName };  
+      } else {
+        book.cover_img = null; 
+      }
+      return book;
+    });
+
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).send({ message: err.message || 'Error while retrieving book information' });
+  }
+};
