f262b1a50fe83239bb975b246f0cf5d3870d1f17
Tola-lemma <tolalemma@gmail.com>
2024-11-23 00:25:34 +0300

    feat:delete books/:id api endpoint to delete books from book store is implemented

--- Route/route.js
+++ Route/route.js
@@ -5,10 +5,12 @@ const getAllBooks = require('../Controller/GetAllBooks')
 const getBooksById = require('../Controller/GetBooksById')
 const coverImg = require('../Controller/GetBooksCoverImgById')
 const bookrecommendation = require('../Controller/BooksRecommendations')
+const deleteBooksById = require('../Controller/deleteBookById')
 //API end-points
 router.post('/books',books.createBooks)
 router.get('/books',getAllBooks.GetAllBooks)
 router.get('/books/:id',getBooksById.GetBookById) 
 router.get('/books/cover/:id',coverImg.GetBookCoverImgById)
 router.get('/books/best/recommendations',bookrecommendation.bookRecommendation)
+router.delete('/books/:id',deleteBooksById.deleteBookById)
 module.exports = router
