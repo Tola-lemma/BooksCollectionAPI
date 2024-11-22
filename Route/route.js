329122f76dbba265cd7a40c69032f74d1b2158af
Tola-lemma <tolalemma@gmail.com>
2024-11-22 22:06:24 +0300

    feat:get /books api endpoint is added

--- Route/route.js
+++ Route/route.js
@@ -1,8 +1,8 @@
 const Router = require('express')
 const router = Router();
 const books  = require('../Controller/BooksController');
-
+const getAllBooks = require('../Controller/GetAllBooks')
 //API end-points
 router.post('/books',books.createBooks)
-
+router.get('/books',getAllBooks.GetAllBooks)
 module.exports = router
