7026d5f8f5bfa16c709efd8f5720dcf6aa30e134
Tola-lemma <tolalemma@gmail.com>
2024-11-22 21:40:04 +0300

    feat:middleware to handle db status for table creation for the table books is implemented

--- Middleware/runDBMigration.js
+++ Middleware/runDBMigration.js
@@ -1,16 +1,17 @@
 const db = require('../Database/connection');
-
+const createBooksTable = require('../Migrations/Books')
 const runDBMigration = async ()=>{
       console.log('BEGIN DB MIGRATION');
       const client = await db.connect();
       try {
             await client.query('BEGIN');
-            
-            await client.query('COMIT');
+            await client.query(createBooksTable);
+            await client.query('COMMIT');
 
             console.log("END MIGRATION");
       } catch (e) {
             await client.query('ROLLBACK')
+            console.error('Migration failed:', e.message);
       }finally{
       client.release();
       }
