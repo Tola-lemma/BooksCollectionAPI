2edf259d9a54af16f998476718856663c347ba04
Tola-lemma <tolalemma@gmail.com>
2024-11-22 22:48:24 +0300

    Fix:error handling incase of db is empty is handled

--- Controller/GetAllBooks.js
+++ Controller/GetAllBooks.js
@@ -4,6 +4,9 @@ exports.GetAllBooks = async (req, res) => {
   const query = 'SELECT * FROM books;';
   try {
     const result = await db.query(query);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'No books found' });
+    }
     const books = result.rows.map(book => {
       if (book.cover_img) {
         const fileName = `cover_${book.id}.jpg`; 
