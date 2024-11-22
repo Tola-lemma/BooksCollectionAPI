9fe122ab33d20f7cce7dd4b5df3cab2df42f6266
Tola-lemma <tolalemma@gmail.com>
2024-11-22 23:28:09 +0300

    feat:get /books/cover/:id api endpoint is added to view cover image

--- Route/route.js
+++ Route/route.js
@@ -3,8 +3,10 @@ const router = Router();
 const books  = require('../Controller/BooksController');
 const getAllBooks = require('../Controller/GetAllBooks')
 const getBooksById = require('../Controller/GetBooksById')
+const coverImg = require('../Controller/GetBooksCoverImgById')
 //API end-points
 router.post('/books',books.createBooks)
 router.get('/books',getAllBooks.GetAllBooks)
 router.get('/books/:id?',getBooksById.GetBookById) 
+router.get('/books/cover/:id',coverImg.GetBookCoverImgById)
 module.exports = router
