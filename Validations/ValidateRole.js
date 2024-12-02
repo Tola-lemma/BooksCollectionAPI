8c005a6a8039a697957c3a71d2bb3d2ec5f68991
Tola-lemma <tolalemma@gmail.com>
2024-12-02 19:25:55 +0300

    feat: role validation middleware is implement for role admin and user only

---
+++ Validations/ValidateRole.js
@@ -0,0 +1,16 @@
+// middlewares/validateRole.js
+const validRoles = [
+      'admin',
+      'user',
+    ];
+    
+    const validateRole = (req, res, next) => {
+      const { role } = req.body;
+      if (!validRoles.includes(role)) {
+        return res.status(400).json({ message: 'Invalid role specified.' });
+      }
+      next();
+    };
+    
+    module.exports = validateRole;
+    
