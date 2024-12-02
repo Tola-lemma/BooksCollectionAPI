a4321aae2ec2b56090538cb685d4545a73f1b904
Tola-lemma <tolalemma@gmail.com>
2024-12-02 19:26:56 +0300

    feat: middleware that handle table creation or migration

--- Middleware/runDBMigration.js
+++ Middleware/runDBMigration.js
@@ -1,11 +1,13 @@
 const db = require('../Database/connection');
 const createBooksTable = require('../Migrations/Books')
+const createUserTable = require('../Migrations/user')
 const runDBMigration = async ()=>{
       console.log('BEGIN DB MIGRATION');
       const client = await db.connect();
       try {
             await client.query('BEGIN');
             await client.query(createBooksTable);
+            await client.query(createUserTable);
             await client.query('COMMIT');
 
             console.log("END MIGRATION");
