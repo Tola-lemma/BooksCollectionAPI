a35bce97825ce9efd62b0af645758aea4d46d617
Tola-lemma <tolalemma@gmail.com>
2024-12-02 19:21:26 +0300

    feat :user table containing username, email, password, role is set

---
+++ Migrations/user.js
@@ -0,0 +1,10 @@
+const createUserTable = `
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20)  NOT NULL CHECK (role IN ('admin','user')), 
+    created_at TIMESTAMP DEFAULT NOW()
+  )`;
+  module.exports = createUserTable;  
