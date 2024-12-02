d4c2df848bac9aa45df9e03056330833bb711734
Tola-lemma <tolalemma@gmail.com>
2024-12-02 19:33:32 +0300

    feat: superadmin is added for latter use

--- Migrations/user.js
+++ Migrations/user.js
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(20)  NOT NULL CHECK (role IN ('admin','user')), 
+    role VARCHAR(20)  NOT NULL CHECK (role IN ('admin','user','superadmin')), 
     created_at TIMESTAMP DEFAULT NOW()
   )`;
   module.exports = createUserTable;  
--- Validations/ValidateRole.js
+++ Validations/ValidateRole.js
@@ -2,6 +2,7 @@
 const validRoles = [
       'admin',
       'user',
+      'superadmin',
     ];
     
     const validateRole = (req, res, next) => {
