e3361c9ba798c0e598a146e4349342fa723f1e6c
Tola-lemma <tolalemma@gmail.com>
2024-12-02 22:47:35 +0300

    feat:role based route is set

--- Route/route.js
+++ Route/route.js
@@ -10,17 +10,24 @@ const updateBookById = require('../Controller/UpdateBookCollection')
 const addToFavorite = require('../Controller/AddtoFavoriteList')
 const Unfavorite = require('../Controller/Unfavorite')
 const FavoriteList = require('../Controller/FavoriteList')
+//middleware
+const generalAuth = require('../Middleware/AuthMiddleware/GeneralAuth')
+const AdminAuth = require('../Middleware/AuthMiddleware/AdminAuthMiddleware')
+const UserAuth = require('../Middleware/AuthMiddleware/UserAuthMiddleware')
 //API end-points
-router.post('/books',books.createBooks)
-router.get('/books',getAllBooks.GetAllBooks)
-router.get('/books/:id',getBooksById.GetBookById) 
-router.get('/books/cover/:id',coverImg.GetBookCoverImgById)
-router.get('/books/best/recommendations',bookrecommendation.bookRecommendation)
-router.delete('/books/:id',deleteBooksById.deleteBookById)
-router.put('/books/:id',updateBookById.updateBookById)
-router.put('/books/add/favorite/:id',addToFavorite.AddtoFavoriteList)
-router.put('/books/unfavorite/:id',Unfavorite.Unfavorite)
-router.get('/books/get/favorite',FavoriteList.FavoriteLists)
+//admin only
+router.post('/books',generalAuth,AdminAuth,books.createBooks)
+router.get('/books',generalAuth,AdminAuth,getAllBooks.GetAllBooks)
+router.delete('/books/:id',generalAuth,AdminAuth,deleteBooksById.deleteBookById)
+router.put('/books/:id',generalAuth,AdminAuth,updateBookById.updateBookById)
+//all
+router.get('/books/:id',generalAuth,getBooksById.GetBookById) 
+router.get('/books/best/recommendations',generalAuth,bookrecommendation.bookRecommendation)
+//user only
+router.get('/books/cover/:id',generalAuth,UserAuth,coverImg.GetBookCoverImgById)
+router.put('/books/add/favorite/:id',generalAuth,UserAuth,addToFavorite.AddtoFavoriteList)
+router.put('/books/unfavorite/:id',generalAuth,UserAuth,Unfavorite.Unfavorite)
+router.get('/books/get/favorite',generalAuth,UserAuth,FavoriteList.FavoriteLists)
 
 //user route
 const signup = require('../Controller/UserController/userController')
