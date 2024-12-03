53a2dfcd3bac6ed4093614c4b9070440506656bd
Tola-lemma <tolalemma@gmail.com>
2024-12-03 18:39:44 +0300

    bug: try to fix d4c2df848bac9aa45df9e03056330833bb711734 commit order

--- Middleware/runDBMigration.js
+++ Middleware/runDBMigration.js
@@ -1,15 +1,15 @@
 const db = require('../Database/connection');
 const createBooksTable = require('../Migrations/Books')
-// const createUserTable = require('../Migrations/user')
-const alterUserTable =require("../Migrations/AlterTableUser")
+const createUserTable = require('../Migrations/user')
+// const alterUserTable =require("../Migrations/AlterTableUser")
 const runDBMigration = async ()=>{
       console.log('BEGIN DB MIGRATION');
       const client = await db.connect();
       try {
             await client.query('BEGIN');
             await client.query(createBooksTable);
-            // await client.query(createUserTable);
-            await client.query(alterUserTable);
+            await client.query(createUserTable);
+            // await client.query(alterUserTable);
             await client.query('COMMIT');
 
             console.log("END MIGRATION");
